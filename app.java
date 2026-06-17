da00152d2f740f2dabf6ae259e64a4a2c18c3f15
ashokjarupula3-hub <ashok.jarupula3@gmail.com>
2026-06-17 15:14:54 +0530

    Update app.java with new code and merge instructions

--- app.java
+++ app.java
@@ -3,4 +3,13 @@ c class HelloWorld {
 		            System.out.println("Hello, World!");
 			        }
 }  
-This is my new code.
+This is my new code.		
+
+
+	//////////////////
+
+	git checkout master
+git merge hello-world-images
+Auto-merging index.html
+CONFLICT (content): Merge conflict in index.html
+Automatic merge failed; fix conflicts and then commit the result.

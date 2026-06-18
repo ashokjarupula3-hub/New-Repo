567db7be082e3226538e2f4456232bb97ed78dab
ashokjarupula3-hub <ashok.jarupula3@gmail.com>
2026-06-18 22:56:11 +0530

    Add HelloWorld class with main method

--- app.java
+++ app.java
@@ -23,3 +23,9 @@ Adding some more info.
 	this is just for testing 
 
 	this is also for testing purpose 
+
+	public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
